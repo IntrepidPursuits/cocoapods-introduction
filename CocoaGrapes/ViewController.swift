d97aeda6c3119b90fdd2124407353b5f851beb4e
Steve Galbraith <stevegalbraith@Steves-MacBook-Air.local>
2017-06-09 15:57:53 -0400

    Add MarqueeLabel pod and implement sweet scrolling label

--- CocoaGrapes/ViewController.swift
+++ CocoaGrapes/ViewController.swift
@@ -7,13 +7,23 @@
 //
 
 import UIKit
+import SwiftGifOrigin
+import MarqueeLabel
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var imageView: UIImageView!
 
+    @IBOutlet weak var marqueeLabel: MarqueeLabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        let dankImage = UIImage.gif(name: "dankgrapes")
+        imageView.image = dankImage
+
+        marqueeLabel.animationCurve = .easeInOut
+
     }
 }
 
